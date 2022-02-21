2fe81a2e4a639136ff76f6bb55ef52a59b25b879
Llubitza-Linares <llubylinares@gmail.com>
2022-02-21 08:42:57 -0400

    segundo commit

--- src/main/java/ejercicio1/Salario.java
+++ src/main/java/ejercicio1/Salario.java
@@ -1,5 +1,18 @@
 package ejercicio1;
 
 public class Salario {
+    public int DevSalario (int sueldo) throws Exception{
+        if (sueldo > 0 && sueldo <= 2000){
+            return sueldo;
+        }else if(sueldo>2000 && sueldo<=4000){
+            return (int)(sueldo*5/100);
+
+        }else if(sueldo>4000){
+            return (int)(sueldo*15/100);
+
+        }else{
+            throw new Exception("El sueldo ingresado es incorrecto!!");
+        }
+    }
 
 }
--- src/test/java/ejercicio1/SalarioTest.java
+++ src/test/java/ejercicio1/SalarioTest.java
@@ -1,4 +1,33 @@
 package ejercicio1;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Locale;
+
 public class SalarioTest {
+    @ParameterizedTest
+    @CsvSource({
+            "0,",
+            "1,1",
+            "2,2",
+
+            "500,500",
+            "501, 501",
+            "502, 502",
+
+            "1999,1999",
+            "2000,2000",
+            "2001, 2001",
+
+    })
+
+    public void descuentoSalario(int sueldo, int expectedResult)throws Exception{
+        Salario salario=new Salario();
+
+        if(sueldo<=0){
+            Assertions.assertThrows(Exception.class, ()->salario.descuento(salario))
+        }
+    }
 }
